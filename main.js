21cb65d2c186252eb94f21b5e47fd2ad5fdfc1d4
Sunday2014 <1906919660@qq.com>
2023-09-06 17:14:24 +0800

    添加节流函数

--- main.js
+++ main.js
@@ -16,3 +16,24 @@ function debounce(fn, delay) {
     }, delay)
   }
 }
+//节流函数
+function throttle(fn, delay) {
+  let timer = null;
+  let startTime = Date.now();
+
+  return function () {
+    let curTime = Date.now();
+    let remaining = delay - (curTime - startTime);
+    let context = this;
+    let args = arguments;
+    clearTimeout(timer);
+    if (remaining <= 0) {
+      fn.apply(context, args);
+      startTime = Date.now();
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(context, args);
+      }, remaining);
+    } 
+  }   
+}
