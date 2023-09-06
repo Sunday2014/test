eb5834bc9f010403ceea976ac0e808edaf67e12e
Sunday2014 <1906919660@qq.com>
2023-09-06 17:08:31 +0800

    添加防抖

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ function hello() {
   console.log("hello world");
 }
 hello();
+
+//防抖函数
+function debounce(fn, delay) {
+  let timer = null;
+
+  return function () {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    setTimeout(() => {
+        fn.apply(this, arguments);
+    }, delay)
+  }
+}
